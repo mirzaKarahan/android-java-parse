4610d6913832491f09805baba36142935a04e42c
Mirza Karahan <mirzaakarahan@gmail.com>
2018-07-18 23:34:57 +0300

    Kayıt ekleme tamamlandı dil eklendi

--- app/src/main/java/com/mirzakarahan/parseplatform/MainActivity.java
+++ app/src/main/java/com/mirzakarahan/parseplatform/MainActivity.java
@@ -10,6 +10,8 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -51,6 +53,33 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(),getApplicationContext().getString(R.string.loginSuccesful)+objects.get(0).getString("username"),Toast.LENGTH_SHORT).show();
                             }else {
                                 Toast.makeText(getApplicationContext(),getApplicationContext().getString(R.string.usernameOrPasswordWrong),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Users");
+                query.whereEqualTo("username",edtUsername.getText().toString().trim());
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e != null){
+                            Toast.makeText(getApplicationContext(),getApplicationContext().getString(R.string.loginError),Toast.LENGTH_SHORT).show();
+                        }else{
+                            if (objects.size()>0){
+                                Toast.makeText(getApplicationContext(),getApplicationContext().getString(R.string.usernameAlreadyExists)+objects.get(0).getString("username"),Toast.LENGTH_SHORT).show();
+                            }else {
+                                //Add user
+                                ParseObject user = new ParseObject("Users");
+                                user.put("username",edtUsername.getText().toString().trim());
+                                user.put("password", edtPassword.getText().toString().trim());
+                                user.saveInBackground();
+                                Toast.makeText(getApplicationContext(),getApplicationContext().getString(R.string.userRegisterSuccessful),Toast.LENGTH_SHORT).show();
                             }
                         }
                     }

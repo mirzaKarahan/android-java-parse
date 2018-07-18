1915e40fed9447d4375008f3bacdb857988f5999
mirzaKarahan <mirzaakarahan@gmail.com>
2018-07-18 23:37:29 +0300

    Update App.java

--- app/src/main/java/com/mirzakarahan/parseplatform/App.java
+++ app/src/main/java/com/mirzakarahan/parseplatform/App.java
@@ -9,9 +9,9 @@ public class App extends Application{
     @Override
     public void onCreate() {
         Parse.initialize(new Parse.Configuration.Builder(this)
-        .applicationId("c1f8e5081e1506137fea6b8f85d40620d50d7d3c")
-        .clientKey("3f1269aea55fd74bdad08e8c2ef853649ce108d3")
-        .server("http://13.58.213.75:80/parse")
+        .applicationId("applicationId")
+        .clientKey("clientKey")
+        .server("http://server/parse")
         .build()
         );
         super.onCreate();
